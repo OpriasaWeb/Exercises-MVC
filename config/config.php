3330c133e82d511fa28b8301ad60a79c4fbf8acd
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-05-03 20:32:37 +0800

    Config folder and file for database connection and message constants

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Define local database
+define("DSN", "mysql:host=localhost;dbname=tutorial");
+define("DBUSER", "root");
+define("DBPASS", "");
+// Define local database
+
+// Define local database reserved to pwc
+define("DSNA", "mysql:host=localhost;dbname=address");
+define("DBUSERA", "root");
+define("DBPASSA", "");
+// Define local database reserved to pwc
+
+// Define pwc database
+define("PWCdsn", "mysql:host=pwc-sapkdbsta57.mysql.database.azure.com;dbname=newtestdb");
+define("PWCUSER", "newtestuser@pwc-sapkdbsta57");
+define("PWCPASS", "nuser08162022!");
+// Define pwc database
+
+?>
---
+++ config/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+class message{
+  const EMPTY_MESSAGE = "Cannot be empty.";
+
+  const INSERTED_SUCCESSFULLY = "Insert record successfully!";
+  const UPDATED_SUCCESSFULLY = "Update record successfully!";
+  const DELETED_SUCCESSFULLY = "Record deleted.";
+  
+  const INSERT_FAILED = "Inserting data failed.";
+  const UPDATED_FAILED = "Update record failed.";
+  const DELETED_FAILED = "Deleting record failed.";
+}
+
+
+
+?>
